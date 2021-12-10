89a760fce4000d4f6de678b7eb087efdeaebb625
Yulia Zherebtsova <juliaz1206@yandex.ru>
2021-12-11 02:34:44 +0300

    feat: верстка главной страницы

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import appStyles from "./app.module.css";
 import "./app.module.css";
+import {
+  Logo,
+  BurgerIcon,
+  ListIcon,
+  ProfileIcon,
+} from "@ya.praktikum/react-developer-burger-ui-components";
 
 function App() {
   return (
     <>
+      <Logo />
       <main className={appStyles.main}>Hello React Burgers</main>
     </>
   );
